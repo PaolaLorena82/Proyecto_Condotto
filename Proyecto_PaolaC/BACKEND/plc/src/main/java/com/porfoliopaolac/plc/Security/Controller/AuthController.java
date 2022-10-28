bbdd5edd8d1c39c783e407f0b5f157436ca09825
PaolaLorena82 <paola_condotto@yahoo.com>
2022-10-28 16:18:30 -0300

    pao
    correcion

--- Proyecto_PaolaC/BACKEND/plc/src/main/java/com/porfoliopaolac/plc/Security/Controller/AuthController.java
+++ Proyecto_PaolaC/BACKEND/plc/src/main/java/com/porfoliopaolac/plc/Security/Controller/AuthController.java
@@ -37,7 +37,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping ("/auth")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendplc-e271a.web.app")
 
 
 public class AuthController {
