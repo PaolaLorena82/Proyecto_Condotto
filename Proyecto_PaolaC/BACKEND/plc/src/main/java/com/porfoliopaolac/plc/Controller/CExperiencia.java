64b00f58e56a4091cfbb3060456b34dfcc9d7ffc
PaolaLorena82 <paola_condotto@yahoo.com>
2022-10-28 16:04:46 -0300

    pao
    Cors

--- Proyecto_PaolaC/BACKEND/plc/src/main/java/com/porfoliopaolac/plc/Controller/CExperiencia.java
+++ Proyecto_PaolaC/BACKEND/plc/src/main/java/com/porfoliopaolac/plc/Controller/CExperiencia.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "https://frontendplc-e271a.web.app")
+
 public class CExperiencia {
 
     @Autowired
